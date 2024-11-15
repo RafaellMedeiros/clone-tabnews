92be1c8324213b205acb96156a1b62a8e996abe8
RafaellMedeiros <64844045+RafaellMedeiros@users.noreply.github.com>
2024-11-15 13:13:32 +0000

    alterando texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 function Home() {
-    return <div>Welcome to Next.js!</div>
+    return (
+        <div className="container">
+            Te amo ❤️ Vi
+            <style jsx>{`
+                .container {
+                    font-size: 4rem;
+                    font-family: Arial, sans-serif;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    background-color: #f0f0f0;
+                }
+            `}</style>
+        </div>
+    );
 }
 
 export default Home;
