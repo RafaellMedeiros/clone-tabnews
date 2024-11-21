c2d28a4a4fd7578e1bc4fd081f8f49598b89393c
Rafael Medeiros <rafaelfufcg@gmail.com>
2024-11-20 22:33:16 -0300

    adicionando arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,6 @@
 function Home() {
     return (
-        <div className="container">
-            Te amo ❤️ Vi
-            <style jsx>{`
-                .container {
-                    font-size: 4rem;
-                    font-family: Arial, sans-serif;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    background-color: #f0f0f0;
-                }
-            `}</style>
-        </div>
+        <h1>Home Page</h1>
     );
 }
 
