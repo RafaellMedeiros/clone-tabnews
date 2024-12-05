6b40a7c423bbe9f16baa050d6b11b06845fdf748
Rafael Medeiros <rafaelfufcg@gmail.com>
2024-12-05 14:01:42 -0300

    feat: enhance status endpoint to include database metrics and error handling

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,35 @@
 import database from "infra/database";
 
 async function status(req, res) {
-  const result = await database.query("SELECT NOW()");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  res.status(200).json({ chave: "Olá Mundo" });
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenendConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenendConnectionsValue =
+    databaseOpenendConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseFloat(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenendConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
