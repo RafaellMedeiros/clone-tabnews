94c2a2eba54b05d03eb9121d3812d621f3033612
Rafael Medeiros <rafaelfufcg@gmail.com>
2024-12-13 09:03:34 -0300

    feat: log environment variables in migrations and status APIs, refactor migration runner configuration

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,36 +1,56 @@
 import migrationRunner from "node-pg-migrate";
+import database from "infra/database";
 import { join } from "path";
 
 export default async function migrations(req, res) {
-  const defaultConfigMigrationRunner = {
-    databaseUrl: process.env.DATABASE_URL,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    dryRun: true,
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  console.log("Environment variables", {
+    NODE_ENV: process.env.NODE_ENV,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_PORT: process.env.POSTGRES_PORT,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_DB: process.env.POSTGRES_DB,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_CA: process.env.POSTGRES_CA,
+  });
+  let dbClient;
 
-  if (req.method === "GET") {
-    const pendingMigrations = await migrationRunner(
-      defaultConfigMigrationRunner,
-    );
+  try {
+    dbClient = await database.getNewClient();
 
-    return res.status(200).json(pendingMigrations);
-  }
+    const defaultConfigMigrationRunner = {
+      dbClient: dbClient,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      dryRun: true,
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  if (req.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultConfigMigrationRunner,
-      dryRun: false,
-    });
+    if (req.method === "GET") {
+      const pendingMigrations = await migrationRunner(
+        defaultConfigMigrationRunner,
+      );
 
-    if (migratedMigrations.length === 0) {
-      return res.status(200).json(migratedMigrations);
+      return res.status(200).json(pendingMigrations);
     }
 
-    return res.status(201).json(migratedMigrations);
-  }
+    if (req.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultConfigMigrationRunner,
+        dryRun: false,
+      });
 
-  return res.status(405).end();
+      if (migratedMigrations.length === 0) {
+        return res.status(200).json(migratedMigrations);
+      }
+
+      return res.status(201).json(migratedMigrations);
+    }
+
+    return res.status(405).end();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await dbClient.end();
+  }
 }
