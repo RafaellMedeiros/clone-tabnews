8251c11ceb6183a6a77e10400e2bc85b7889c6ae
Rafael Medeiros <rafaelfufcg@gmail.com>
2024-12-13 08:17:36 -0300

    returning code status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,38 +1,34 @@
 import database from "infra/database";
 
 async function status(req, res) {
-  // const updatedAt = new Date().toISOString();
-  // const databaseVersionResult = await database.query("SHOW server_version");
-  // const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  // const databaseMaxConnectionsResult = await database.query(
-  //   "SHOW max_connections",
-  // );
-  // const databaseMaxConnectionsValue =
-  //   databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
-  // const databaseName = process.env.POSTGRES_DB;
-  // const databaseOpenendConnectionsResult = await database.query({
-  //   text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-  //   values: [databaseName],
-  // });
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenendConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
 
-  // const databaseOpenendConnectionsValue =
-  //   databaseOpenendConnectionsResult.rows[0].count;
-
-  // res.status(200).json({
-  //   updated_at: updatedAt,
-  //   dependencies: {
-  //     database: {
-  //       version: databaseVersionValue,
-  //       max_connections: parseFloat(databaseMaxConnectionsValue),
-  //       opened_connections: databaseOpenendConnectionsValue,
-  //     },
-  //   },
-  // });
+  const databaseOpenendConnectionsValue =
+    databaseOpenendConnectionsResult.rows[0].count;
 
   res.status(200).json({
-    env: process.env,
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseFloat(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenendConnectionsValue,
+      },
+    },
   });
 }
 
